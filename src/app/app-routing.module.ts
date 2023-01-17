1d60ff25ecd3e119a31f09e56b927569ad99e768
Josue Ale <jam9814@hotmail.com>
2023-01-17 17:14:23 -0500

    initial commit -m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+  },
+
+  {
+    path: 'dashboard',
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
