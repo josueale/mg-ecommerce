819b569511f478cf4111370befa75606f729ccbc
Josue Ale <jam9814@hotmail.com>
2023-01-18 11:53:12 -0500

    added login page

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  isValidField(field: string) {
+    return (
+      !this.loginForm.controls[field].valid &&
+      this.loginForm.controls[field].touched
+    );
+  }
+
+
+  handleOnSubmit({ value: form }: FormGroup) {
+    // TODO validate answer
+    // ?  redirect to login???
+
+    // this.authSvc.login(form).subscribe();
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
