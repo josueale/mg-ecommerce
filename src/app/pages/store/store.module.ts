9e2af8a1931690818c8bc6ca5875681c2b656872
Josue Ale <jam9814@hotmail.com>
2023-01-20 10:12:43 -0500

    update

--- src/app/pages/store/store.module.ts
+++ src/app/pages/store/store.module.ts
@@ -5,12 +5,14 @@ import { NavbarModule } from 'src/app/shared/components/navbar/navbar.module';
 import { LandingStoreComponent } from './pages/landing-store/landing-store.component';
 import { StoreRoutingModule } from './store-routing.module';
 import { StoreComponent } from './store.component';
+import { ProductCardComponent } from './pages/landing-store/components/product-card/product-card.component';
 
 
 @NgModule({
   declarations: [
     StoreComponent,
     LandingStoreComponent,
+    ProductCardComponent,
 
   ],
   imports: [
