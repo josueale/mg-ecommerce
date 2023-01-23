bf70029016016405b45d9df270577827b1c07181
Josue Ale <jam9814@hotmail.com>
2023-01-23 16:29:28 -0500

    feature: added logout @ user dashboard

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
 })
 export class UserComponent {
+
+constructor(
+  private authSvc: AuthService,
+  ){}
+
+
+  handleLogout(){
+    this.authSvc.logout()
+  }
 
 }
