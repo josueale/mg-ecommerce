a8b6aea6764f4c176c2cd5de0a2ca7f9fce93180
Josue Ale <jam9814@hotmail.com>
2023-01-24 08:25:30 -0500

    fix directories

--- src/app/pages/dashboard/pages/products/products-routing.module.ts
+++ src/app/pages/dashboard/pages/products/products-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NewProductComponent } from './pages/new-product/new-product.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
 const routes: Routes = [
   // Only support ['edit', 'view']
   { path: '', component: ProductListComponent },
-  { path: 'new', component: ProductListComponent },
+  { path: 'new', component: NewProductComponent },
   { path: ':action/:id', component: ProductListComponent },
 ];
 
--- src/app/pages/dashboard/pages/products/products.module.ts
+++ src/app/pages/dashboard/pages/products/products.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NewProductComponent } from './pages/new-product/new-product.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ProductsRoutingModule } from './products-routing.module';
-import { NewProductComponent } from './pages/product-list/components/new-product/new-product.component';
 
 
 @NgModule({
