0ac48c919690ee065da32480bd83d75c73f9845f
Josue Ale <jam9814@hotmail.com>
2023-01-24 08:48:11 -0500

    update: breadcrumb

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+})
+export class BreadcrumbComponent {
+
+}
