6daa6f2dbc8a1e5720033b15c9734365a0796565
Josue Ale <jam9814@hotmail.com>
2023-01-25 16:40:49 -0500

    update: cart list

--- src/app/pages/store/pages/cart/pages/cart-list/cart-list.component.ts
+++ src/app/pages/store/pages/cart/pages/cart-list/cart-list.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/shared/services/cart.service';
 
 @Component({
   selector: 'app-cart-list',
   templateUrl: './cart-list.component.html',
 })
 export class CartListComponent {
+
+  constructor(
+    private cartSrv: CartService,
+  ) { }
+
+
+  get cart() {
+    return this.cartSrv.cart
+  }
 
 }
